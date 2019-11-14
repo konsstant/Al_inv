f9ae2fc9775de9bc707e1a722aea800bc63ff605
konsstant <56802524+konsstant@users.noreply.github.com>
2019-11-14 23:21:51 +0200

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,14 @@ from pygame.locals import *
 from setting import Settings
 from ship import Ship
 import game_functions as gf
+import bullet
+from pygame.sprite import Group
+import Alien
+from Alien import Alien
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
 
 
 def run_game():
@@ -14,30 +22,34 @@ def run_game():
     screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship=Ship(screen)
+    play_button=Button(ai_settings,screen,"Play")
+
+    ship=Ship(ai_settings,screen)
+
+    bullets=Group()
+
+    aliens=Group()
+
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+
+    stats=GameStats(ai_settings)
+
+    sb=Scoreboard(ai_settings,screen,stats)
 
     bg_fill=(230,230,230)
     Gameexit=True
 
+    alient=Alien(ai_settings,screen)
+
     while Gameexit:
 
-        #screen.fill(ai_settings.bg_color)
+        gf.check_events(ai_settings,screen,ship,bullets,stats,play_button,aliens,sb)
 
-        #ship.blitme()
-
-        gf.check_events(ship)
-
-        #for event in pygame.event.get():
-        #   if event.type==QUIT:
-        #       sys.exit()
-
-        #pygame.display.flip()
-        gf.update_screen(ai_settings,screen,ship)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,bullets,aliens,sb,stats)
+            gf.update_aliens(ai_settings,stats,screen,aliens,ship,bullets,sb)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets,play_button,stats,sb)
 
 
 run_game()
-
-
-#display_test()
-
-#def Kaboom():
--- ship.py
+++ ship.py
@@ -1,19 +1,46 @@
 import pygame
+from pygame.sprite import Sprite
+
+class Life (pygame.sprite.Sprite):
+
+    def __init__(self,ai_settings,screen):
+        super(Life,self).__init__()
+        self.image = pygame.image.load('ship_01.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
+
         self.screen=screen
 
-        self.image=pygame.image.load('ship.png')
+        self.ai_settings=ai_settings
+
+        self.image=pygame.image.load('ship_00.png')
         self.rect=self.image.get_rect()
         self.screen_rect=screen.get_rect()
 
         self.rect.centerx=self.screen_rect.centerx
         self.rect.bottom=self.screen_rect.bottom
-        #new next page 242
 
+        self.moving_right=False
+        self.moving_left = False
 
+        self.center=float(self.rect.centerx)
+
+    def update(self):
+        if self.moving_right and self.rect.right<self.screen_rect.right :
+            #self.rect.centerx+=1
+            self.center+=self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left>0 :
+            #self.rect.centerx-=1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx=self.center
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
+
+    def center_ship(self):
+        self.center=self.screen_rect.centerx
